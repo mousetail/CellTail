bc8379824dc8d1bb1ec71d9aac0a10924302cdc2
Maurits van Riezen (mousetail) <mousetail@mousetail.nl>
2022-08-27 21:51:28 +0200

    prevent functions from calling other functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 
+mod checker;
 mod errors;
 mod interpreter;
 mod lexer;
@@ -12,6 +13,8 @@ fn parse_and_run_code(code: &Vec<char>) -> errors::CellTailResult<()> {
     let tokens = tokenizer::tokenize(code)?;
     let lexical_tokens = lexer::lex(tokens)?;
     let structure = parser::parse(lexical_tokens)?;
+
+    checker::check_program(&structure)?;
 
     if structure.attributes.debug {
         println!("{:?}", structure);
